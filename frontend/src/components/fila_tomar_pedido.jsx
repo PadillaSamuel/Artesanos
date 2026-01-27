e80eabebc5907a5b03206d3a637ed68b9c600749
yulian168 <ym67896@gmail.com>
2026-01-27 15:24:11 -0500

    ventana-tomar-pedidos

---
+++ frontend/src/components/fila_tomar_pedido.jsx
@@ -0,0 +1,17 @@
+import './fila_tomar_pedido.css'
+
+const FilaTomarPedido=()=>{
+    return(
+        <>
+            <section className='fila-tomar-pedido-sec'>
+                <div className='fila-tomar-pedido-div-uno'>1</div>  
+                <div className='fila-tomar-pedido-div-dos'>Pizza Espanta Brujas</div>  
+                <div className='fila-tomar-pedido-div-tres'>+</div>  
+                <div className='fila-tomar-pedido-div-cuatro'>-</div>  
+                <div className='fila-tomar-pedido-div-cinco'>$20.000</div>  
+            </section>
+        </>
+    )
+}
+
+export default FilaTomarPedido
--- frontend/src/pages/tomar_pedido.jsx
+++ frontend/src/pages/tomar_pedido.jsx
@@ -1,39 +1,53 @@
 import './tomar_pedido.css'
 import arrow from '../assets/flecha.png'
+import FilaTomarPedido from '../components/fila_tomar_pedido'
 
-const TomarPedido= ()=>{
-    return(
+const TomarPedido = () => {
+    return (
         <>
             <section className='tomar-pedido-sec'>
-                <div className='tomar-pedido-div-uno'>
-                    <button className='tomar-pedido-boton-volver'>
-                        <img src={arrow} alt="" />
-                    </button>
-                </div>
-                <div className='tomar-pedido-div-dos'>
-                    <label htmlFor="">Número de mesa</label>
-                    <input type="text" placeholder='Ej: mesa 1'/>
-                </div>
-                <div className='tomar-pedido-div-tres'>
+                <div className='main'>
+                    <div className='tomar-pedido-div-uno'>
+                        <button className='tomar-pedido-boton-volver'>
+                            <img src={arrow} alt="" />
+                        </button>
+                    </div>
+                    <div className='tomar-pedido-div-dos'>
+                        <label htmlFor="" className='numero-mesa'>Número de mesa</label>
+                        <input type="text" placeholder='Ej: mesa 1' className='input-mesa' />
+                    </div>
+                    <div className='tomar-pedido-div-tres'>
 
-                    <div>
-                        <h3>Pedido</h3>
+                        <div>
+                            <h3>Pedido</h3>
+                        </div>
+
+                        <div className='items-pedido'>
+                            <FilaTomarPedido/>
+                            <FilaTomarPedido/>
+                            <FilaTomarPedido/>
+                            <FilaTomarPedido/>
+                            <FilaTomarPedido/>
+                            <FilaTomarPedido/>
+                        </div>
+
+                    </div>
+                    <div className='tomar-pedido-div-cuatro'>
+                        <button>Añadir producto</button>
+                    </div>
+                </div>
+
+                <footer className='tomar-pedido-div-cinco'>
+                    <div className='div-confirmar-pedido'>
+                        <button className='button-confirmar-pedido'>Confirmar pedido</button>
+                        <button className='button-anular-pedido'>Anular pedido</button>
                     </div>
 
-                    <div>
-                    
-                    </div>
-
-                </div>
-                <div className='tomar-pedido-div-cuatro'>
-                    <button>Añadir producto</button>
-                </div>
-                <div className='tomar-pedido-div-cinco'>
-                    <button>Confirmar pedido</button>
                     <h3>Total</h3>
-                    <h3>$</h3>
-                </div>
+                    <h3>$0</h3>
+                </footer>
             </section>
+
         </>
     )
 }
